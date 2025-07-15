63bd73521a7e66e307850e0cc6bdc21a31feb5dc
Mati658 <matiasaltamira@outlook.com>
2025-07-15 17:55:25 -0300

    verificando variables

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -16,9 +16,6 @@ export default function Login() {
 
     useEffect(()=>{
         verificarUsuario();
-
-        console.log(import.meta.env.VITE_GOOGLE_ID_CLIENTE)
-        console.log(import.meta.env.VITE_SUPABASE_URL)
     },[])
 
     const verificarUsuario = async() =>{
@@ -53,35 +50,40 @@ export default function Login() {
     };
 
     return (
-        <GoogleOAuthProvider clientId={import.meta.env.GOOGLE_ID_CLIENTE}>
-            <div className='container-page-login'>
+        <>
+        {import.meta.env.GOOGLE_ID_CLIENTE &&
+            <GoogleOAuthProvider clientId={import.meta.env.GOOGLE_ID_CLIENTE}>
+                <div className='container-page-login'>
 
-                <dialog ref={dialogRef} className='msj-error'>
-                    <p>{mensaje}</p>
-                    <button onClick={cerrarModal}>Continuar</button>
-                </dialog>
+                    <dialog ref={dialogRef} className='msj-error'>
+                        <p>{mensaje}</p>
+                        <button onClick={cerrarModal}>Continuar</button>
+                    </dialog>
 
 
-                <form className="form">
-                    <p className="title inter">Iniciar sesión</p>
+                    <form className="form">
+                        <p className="title inter">Iniciar sesión</p>
 
-                    <p className="message inter">¡Inicia sesión para poder comprar! </p>
+                        <p className="message inter">¡Inicia sesión para poder comprar! </p>
+                                
+                        <label>
+                            <input required={true} placeholder="" type="email" className="input" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                            <span>Email</span>
+                        </label> 
                             
-                    <label>
-                        <input required={true} placeholder="" type="email" className="input" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-                        <span>Email</span>
-                    </label> 
-                        
-                    <label>
-                        <input required={true} placeholder="" type="password" className="input" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-                        <span>Contraseña</span>
-                    </label>
-                
-                    <button className="submit" type='button' onClick={loguear}>Inciar Sesión</button>
-                    <p className="signin">¿No tenés una cuenta? <a href="/registro">Regsitrar</a> </p>
-                    <GoogleLogin useOneTap onSuccess={handleSuccess} onError={()=>console.log('Error al iniciar sesion en Google')}></GoogleLogin>
-                </form>
-            </div>
-        </GoogleOAuthProvider>
+                        <label>
+                            <input required={true} placeholder="" type="password" className="input" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                            <span>Contraseña</span>
+                        </label>
+                    
+                        <button className="submit" type='button' onClick={loguear}>Inciar Sesión</button>
+                        <p className="signin">¿No tenés una cuenta? <a href="/registro">Regsitrar</a> </p>
+                        <GoogleLogin useOneTap onSuccess={handleSuccess} onError={()=>console.log('Error al iniciar sesion en Google')}></GoogleLogin>
+                    </form>
+                </div>
+            </GoogleOAuthProvider>
+            
+        }
+        </>
     )
 }
